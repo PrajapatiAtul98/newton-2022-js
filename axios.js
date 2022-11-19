b24f5cf0838c44a9a34917ea9a0666741fd8baed
ATUL PRAJAPATI <114531142+PrajapatiAtul98@users.noreply.github.com>
2022-11-19 17:58:42 +0530

    Update axios.js

--- axios.js
+++ axios.js
@@ -26,3 +26,24 @@ axios.post("https://jsonplaceholder.typicode.com/posts", {
     body: 'bar',
     userId: 1,
   }).then((response) => console.log(response))
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script defer src="axios.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios@1.1.2/dist/axios.min.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
